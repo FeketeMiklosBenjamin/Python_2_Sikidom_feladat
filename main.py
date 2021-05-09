f2b114581b87775b32bee2b19ab2ec19ee26ee75
FeketeMiklosBenjamin <fekete.miklos.benjamin@students.jedlik.eu>
2021-05-09 18:18:26 +0200

    A rombusz területe

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ def main() -> None:
     else:
         rombusz_kerület: float = 4 * a
         print(f'A rombusz kerülete: {rombusz_kerület}')
+
+    print('A rombusz területét kiszámoló függvény!')
+    e : float = float(input('e (átló) = '))
+    f : float = float(input('f (átló) = '))
+    if e < 1 or f < 1:
+        print('A megadott adatokkal nem lehet számolni!')
+    else:
+        rombusz_terület: float = (e + f) / 2
+        print(f'A rombusz területe: {rombusz_terület}')
